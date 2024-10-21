bb7837f43ca9dce2eea50b8f758a64770da4ca38
Jehna Sinamban <jehnasinamban@gmail.com>
2024-10-21 14:35:19 -0400

    Added CircleTests.java

---
+++ CircleTests.java
@@ -0,0 +1,99 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CircleTests {
+	
+	@Test
+	void testCircle() {
+		Circle c1 = new Circle(1, 0, 0);
+		assertTrue(c1.x == 0 && c1.y == 0 && c1.radius == 1);
+	}
+	
+	@Test
+	void testGetRadius() {
+		Circle c1 = new Circle(5, 1, 2);
+		double r = c1.getRadius();
+		assertEquals(r, 5);
+	}
+	
+	@Test
+	void testSetRadius() {
+		Circle c1 = new Circle(2,2,2);
+		c1.setRadius(7);
+		assertEquals(c1.radius, 7);
+	}
+	
+	@Test
+	void testGetDiameter() {
+		Circle c1 = new Circle(3,1,0);
+		assertEquals(c1.getDiameter, 6);
+	}
+	
+	@Test
+	void testGetArea() {
+		Circle c1 = new Circle(1,0,0);
+		assertEquals(c1.getArea(), Math.PI);
+	}
+	
+	@Test
+	void testGetPerimeter() {
+		Circle c1 = new Circle(1,2,8);
+		assertEquals(c1.getPerimeter(), 2*Math.PI);
+	}
+	
+	@Test
+	void testHashCode() {
+		Circle c1 = new Circle(1,4,4);
+		Circle c2 = new Circle(1,4,4);
+		assertEquals(c1.hashcode(),c2.hashcode);
+	}
+	
+	@Test
+	void testEquals1() {
+		Circle c1 = new Circle(2,0,0);
+		assertTrue(c1.equals(c1));
+	}
+	
+	@Test
+	void testEquals2() {
+		Circle c1 = new Circle(2,0,0);
+		Circle c2 = null;
+		assertNotTrue(c1.equals(c2));
+	}
+	
+	@Test
+	void testEquals3() {
+		Circle c1 = new Circle(2,0,0);
+		int x = 5;
+		assertNotTrue(c1.equals(x));
+	}
+	
+	@Test
+	void testEquals4() {
+		Circle c1 = new Circle(2,0,0);
+		Circle c2 = new Circle(4,1,0);
+		assertNotTrue(c1.equals(c2));
+	}
+	
+	@Test
+	void testEquals5() {
+		Circle c1 = new Circle(2,0,0);
+		Circle c2 = new Circle(4,0,1);
+		assertNotTrue(c1.equals(c2));
+	}
+	
+	@Test
+	void testEquals6() {
+		Circle c1 = new Circle(2,0,0);
+		Circle c2 = new Circle(4,0,0);
+		assertNotTrue(c1.equals(c2));
+	}
+	
+	@Test
+	void testEquals7() {
+		Circle c1 = new Circle(2,0,0);
+		Circle c2 = new Circle(2,0,0);
+		assertTrue(c1.equals(c2));
+	}
+}
