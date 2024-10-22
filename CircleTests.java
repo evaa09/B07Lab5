065d16d0db945bffe4f276f302d0a9d73bb63f2d
Jehna Sinamban <jehnasinamban@gmail.com>
2024-10-22 08:41:49 -0400

    Fixed errors in CircleTests.java

--- CircleTests.java
+++ CircleTests.java
@@ -27,7 +27,7 @@ class CircleTests {
 	@Test
 	void testGetDiameter() {
 		Circle c1 = new Circle(3,1,0);
-		assertEquals(c1.getDiameter, 6);
+		assertEquals(c1.getDiameter(), 6);
 	}
 	
 	@Test
@@ -46,7 +46,7 @@ class CircleTests {
 	void testHashCode() {
 		Circle c1 = new Circle(1,4,4);
 		Circle c2 = new Circle(1,4,4);
-		assertEquals(c1.hashcode(),c2.hashcode);
+		assertEquals(c1.hashCode(),c2.hashCode());
 	}
 	
 	@Test
@@ -59,35 +59,35 @@ class CircleTests {
 	void testEquals2() {
 		Circle c1 = new Circle(2,0,0);
 		Circle c2 = null;
-		assertNotTrue(c1.equals(c2));
+		assertTrue(!(c1.equals(c2)));
 	}
 	
 	@Test
 	void testEquals3() {
 		Circle c1 = new Circle(2,0,0);
 		int x = 5;
-		assertNotTrue(c1.equals(x));
+		assertTrue(!(c1.equals(x)));
 	}
 	
 	@Test
 	void testEquals4() {
 		Circle c1 = new Circle(2,0,0);
 		Circle c2 = new Circle(4,1,0);
-		assertNotTrue(c1.equals(c2));
+		assertTrue(!(c1.equals(c2)));
 	}
 	
 	@Test
 	void testEquals5() {
 		Circle c1 = new Circle(2,0,0);
 		Circle c2 = new Circle(4,0,1);
-		assertNotTrue(c1.equals(c2));
+		assertTrue(!(c1.equals(c2)));
 	}
 	
 	@Test
 	void testEquals6() {
 		Circle c1 = new Circle(2,0,0);
 		Circle c2 = new Circle(4,0,0);
-		assertNotTrue(c1.equals(c2));
+		assertTrue(!(c1.equals(c2)));
 	}
 	
 	@Test
